e029929d5ee67fe10eb08761fd709bff8b3bebd7
Alejandro Perdomo <diegoleandro-29@hotmail.com>
2021-07-25 08:39:32 -0500

    Se añade otras propiedaes como temp max y temp minima, también un spinner

--- js/app.js
+++ js/app.js
@@ -58,6 +58,7 @@ function consultarAPI(ciudad,pais){
     const appId = '568fc38779400334e340eee345300d0c';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
     
+    Spinner();
 
     fetch(url)
     .then(resp => resp.json())
@@ -76,15 +77,35 @@ function consultarAPI(ciudad,pais){
 
 function mostrarClima(datos){
     
-    const {main:{temp, temp_max, temp_min}} = datos;
+    const {name, main:{temp, temp_max, temp_min}} = datos;
     const centigrados = kelvinCentigrados(temp);
+    const max = kelvinCentigrados(temp_max);
+    const min = kelvinCentigrados(temp_min);
+
+    const nombreCiudad = document.createElement('P');
+    nombreCiudad.textContent = `Clima en ${name}`;
+    nombreCiudad.classList.add('font-bold', 'text-2xl');
+
     const actual = document.createElement('P');
     actual.innerHTML = `${centigrados} &#8451;`;
     actual.classList.add('font-bold', 'text-6xl');
 
+
+    const tempMax = document.createElement('P');
+    tempMax.innerHTML = `Max: ${max} &#8451;`;
+    tempMax.classList.add('text-xl');
+
+    const tempMin = document.createElement('P');
+    tempMin.innerHTML = `Min: ${min} &#8451;`;
+    tempMin.classList.add('text-xl');
+
     const resultadoDiv = document.createElement('DIV');
     resultadoDiv.classList.add('text-center');
+    resultadoDiv.appendChild(nombreCiudad);
     resultadoDiv.appendChild(actual);
+    resultadoDiv.appendChild(tempMax);
+    resultadoDiv.appendChild(tempMin);
+    
 
     resultado.appendChild(resultadoDiv);
 }
@@ -96,4 +117,27 @@ function limpiarHTML(){
     while(resultado.firstChild){
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function Spinner(){
+    limpiarHTML();
+    const divSpinner = document.createElement('DIV');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 }
